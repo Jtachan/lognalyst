1270fadcbed6d16a77e22a0a823426c42975eae0
gonzal06 <jaime.gonzalez-gomez@ptb.de>
2026-03-12 14:41:36 +0100

    Feature: Defined config (input file path) and displaying the contents of any file

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod input;
+
+use std::{env, process, fs};
+use std::error::Error;
+use input::Config;
+
+fn read_file(file_path: &str) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(file_path)?;
+    println!("{}", contents);
+    Ok(())
+}
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments -> {err}");
+        process::exit(1);
+    });
+    if let Err(e) = read_file(&config.file_path) {
+        eprintln!("[Error] -- {e}");
+        process::exit(1);
+    }
+}
