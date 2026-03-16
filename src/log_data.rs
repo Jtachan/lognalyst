5e71194b72f069df4be969fba48fc836b6215803
gonzal06 <jaime.gonzalez-gomez@ptb.de>
2026-03-16 13:55:25 +0100

    LogLevel: Missing level 'Critical' added

--- src/log_data.rs
+++ src/log_data.rs
@@ -3,10 +3,11 @@
 /// Enumeration for all the valid log levels that can be contained within a log file.
 #[derive(Debug, Clone, PartialEq)]
 pub enum LogLevel {
-    INFO,
-    ERROR,
-    WARN,
-    DEBUG,
+    Debug,
+    Info,
+    Warning,
+    Error,
+    Critical,
 }
 
 impl LogLevel {
@@ -15,17 +16,19 @@ impl LogLevel {
     /// # Example
     /// ```
     /// let info_level = LogLevel::from_str("info").unwrap();
-    /// dbg!(info_level);  // LogLevel::INFO
+    /// dbg!(info_level);  // LogLevel::Info
     ///
     /// let wrong_level = LogLevel::from_str("invalid");
     /// dgb!(wrong_level)  // None
     /// ```
     pub fn from_str(s: &str) -> Option<Self> {
         match s.to_uppercase().as_str() {
-            "INFO" => Some(LogLevel::INFO),
-            "ERROR" => Some(LogLevel::ERROR),
-            "WARN" => Some(LogLevel::WARN),
-            "DEBUG" => Some(LogLevel::DEBUG),
+            "DEBUG" => Some(LogLevel::Debug),
+            "INFO" => Some(LogLevel::Info),
+            "WARN" => Some(LogLevel::Warning),
+            "WARNING" => Some(LogLevel::Warning),
+            "ERROR" => Some(LogLevel::Error),
+            "CRITICAL" => Some(LogLevel::Critical),
             _ => None,
         }
     }
@@ -48,10 +51,11 @@ pub struct LogEntry<'a> {
 impl LogEntry<'_> {
     pub fn display(&self) {
         let level_name = match &self.level {
-            LogLevel::INFO => "INFO",
-            LogLevel::ERROR => "ERROR",
-            LogLevel::WARN => "WARN",
-            LogLevel::DEBUG => "DEBUG",
+            LogLevel::Debug => "DEBUG",
+            LogLevel::Info => "INFO",
+            LogLevel::Warning => "WARNING",
+            LogLevel::Error => "ERROR",
+            LogLevel::Critical => "CRITICAL",
         };
 
         println!("-----------\nLog Entry\n-----------");
@@ -70,10 +74,11 @@ mod tests {
 
     #[test]
     fn log_level_from_str() {
-        assert_eq!(LogLevel::from_str("INFO").unwrap(), LogLevel::INFO);
-        assert_eq!(LogLevel::from_str("ErrOr").unwrap(), LogLevel::ERROR);
-        assert_eq!(LogLevel::from_str("warn").unwrap(), LogLevel::WARN);
-        assert_eq!(LogLevel::from_str("DEBug").unwrap(), LogLevel::DEBUG);
+        assert_eq!(LogLevel::from_str("DEBug").unwrap(), LogLevel::Debug);
+        assert_eq!(LogLevel::from_str("Info").unwrap(), LogLevel::Info);
+        assert_eq!(LogLevel::from_str("warn").unwrap(), LogLevel::Warning);
+        assert_eq!(LogLevel::from_str("ErrOr").unwrap(), LogLevel::Error);
+        assert_eq!(LogLevel::from_str("CRITICAL").unwrap(), LogLevel::Critical);
         assert_eq!(LogLevel::from_str("other"), None);
     }
 }
